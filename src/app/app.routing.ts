ea8c437d4e7337e9facfa9782a2f4dd3dd093045
Arturo Villa <afve92@gmail.com>
2019-08-29 20:25:48 -0500

    ruta de login

---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { NgModule, Component } from "@angular/core";
+import { Routes,RouterModule } from "@angular/router";
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [{  
+
+    path:'login',
+    component:LoginComponent
+
+}]
+
+@NgModule({
+
+    imports:[ RouterModule.forRoot(routes,{
+
+        useHash:true
+
+    }) ],
+    exports:[ 
+        RouterModule 
+],
+    providers:[]
+
+
+})
+
+export class AppRoutingModule{
+
+}
