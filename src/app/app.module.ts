3349456762f2fb73079bc5cb36f25e9daf062ccf
Arturo Villa <afve92@gmail.com>
2019-09-05 20:25:40 -0500

    modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app.routing';
+import { ThemeComponent } from './theme/theme.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ThemeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,28 +1,27 @@
-import { NgModule, Component } from "@angular/core";
-import { Routes,RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import {Routes,RouterModule, Router} from "@angular/router";
 import { LoginComponent } from './login/login.component';
-export const routes: Routes = [{  
-
-    path:'login',
-    component:LoginComponent
-
-}]
-
-@NgModule({
-
-    imports:[ RouterModule.forRoot(routes,{
-
-        useHash:true
-
-    }) ],
-    exports:[ 
-        RouterModule 
-],
-    providers:[]
-
-
-})
-
-export class AppRoutingModule{
+import { ThemeComponent } from './theme/theme.component';
+export const routes: Routes=[{
+    path  : 'login',    
+    component : LoginComponent
+},
+{
+    path: 'pages',
+    component: ThemeComponent,
+    children: [
+        {
+            path: '',
+            loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'
+        }
+    ]
 
 }
+]
+@NgModule({
+imports:[ RouterModule.forRoot(routes,{useHash:true})
+],
+exports:[RouterModule],
+providers:[]
+}) 
+export class AppRoutingModule {}
